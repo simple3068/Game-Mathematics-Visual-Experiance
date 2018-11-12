5e2557fa0c2792f31b592cb096dd43aa03211ae8
Simple <simple3068@naver.com>
2018-11-12 14:09:52 +0900

    Ch. 09 Motion in 2D and 3D
    * 고무신 던지기 비슷한 게임을 구현합니다.
    * 플레이어는 번갈아가며 포탄을 발사합니다.
    * 이 때 플레이어는 각도와 시작 속도를 조절할 수 있습니다.
    * 키보드 좌우(Axis:Horizontal)키와 스페이스바를 이용하세요.

---
+++ Assets/Visual Experiance/Chapter 09/Scripts/Chapter09BulletBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter09BulletBehaviour : MonoBehaviour
+{
+    [SerializeField] int nBulletPower;
+
+    public string strStartName;
+
+    Chapter09Controller ctrl;
+
+
+    void Start ()
+    {
+        ctrl = GameObject.FindGameObjectWithTag("GameController").GetComponent<Chapter09Controller>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Terrain"))
+        {
+            ctrl.BulletHit();
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Player 1") && other.tag != strStartName)
+        {
+            ctrl.nPlayer1HP -= nBulletPower;
+            ctrl.BulletHit();
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Player 2") && other.tag != strStartName)
+        {
+            ctrl.nPlayer2HP -= nBulletPower;
+            ctrl.BulletHit();
+            Destroy(gameObject);
+        }
+    }
+}
