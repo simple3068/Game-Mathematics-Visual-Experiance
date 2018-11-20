fe3f60cb6e53527a2c9265517858706a2c836f1b
Simple <simple3068@naver.com>
2018-11-20 17:26:27 +0900

    Ch. 03 Trigonometry
    * 각도에 따른 삼각함수의 결과를
       확인할 수 있는 시각체험입니다.
    * 우측 슬라이더로 각도를 지정하거나
    * 우측 하단의 자동 회전 버튼으로
       각도를 자동으로 변경해줍니다.
    * 좌측 슬라이더로 함수의 진폭을
       바꿔볼 수도 있겠습니다.
    * 참 재미있겠지요?

---
+++ Assets/Visual Experience/Chapter 03/Scripts/Chapter03Controller.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chapter03Controller : MonoBehaviour
+{
+    [SerializeField] Transform trsCubeSin;
+    [SerializeField] Transform trsCubeCos;
+    [SerializeField] Transform trsCubeTan;
+    [Space]
+    [SerializeField] Slider sldAngle;
+    [SerializeField] Text txtAngle;
+    [SerializeField] Text txtSin;
+    [SerializeField] Text txtCos;
+    [SerializeField] Text txtTan;
+    [Space]
+    [SerializeField] Slider sldAmp;
+    [SerializeField] Text txtAmp;
+    [Space]
+    [SerializeField] Transform trsAmpRange;
+    [Space]
+    [SerializeField] Button btnAutoRotation;
+
+    bool bAutoRotation;
+
+    float fSin;
+    float fCos;
+    float fTan;
+
+    float fAmp = 1.0f;
+
+    float fAngle;
+    float fAngleInRad;
+
+    void Start ()
+    {
+        btnAutoRotation.onClick.AddListener(delegate
+        {
+            bAutoRotation = !bAutoRotation;
+        });
+	}
+
+    void Update()
+    {
+        // Code about Angle
+        if (bAutoRotation)
+        {
+            btnAutoRotation.transform.localEulerAngles += new Vector3(0.0f, 0.0f, -1.0f);
+
+            fAngle = Mathf.Repeat(fAngle + 1.0f, 360.0f);
+
+            sldAngle.value = fAngle;
+        }
+        else
+        {
+            btnAutoRotation.transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+
+            fAngle = sldAngle.value;
+        }
+
+        // Code about Angle
+        fAngleInRad = fAngle * Mathf.Deg2Rad;
+        txtAngle.text = fAngle.ToString("0.0000");
+
+        // Code about Amplitude
+        fAmp = sldAmp.value;
+        txtAmp.text = fAmp.ToString("0.0000");
+
+        trsAmpRange.localScale = new Vector3(trsAmpRange.localScale.x, fAmp * 2.0f, trsAmpRange.localScale.z);
+
+        // Calculate Trigonometric Funcs
+        fSin = fAmp * Mathf.Sin(fAngleInRad);
+        fCos = fAmp * Mathf.Cos(fAngleInRad);
+        fTan = fAmp * Mathf.Tan(fAngleInRad);
+
+        // Apply calculation result to cube
+        trsCubeSin.position = new Vector3(trsCubeSin.position.x, fSin, trsCubeSin.position.z);
+        trsCubeCos.position = new Vector3(trsCubeCos.position.x, fCos, trsCubeCos.position.z);
+        trsCubeTan.position = new Vector3(trsCubeTan.position.x, fTan, trsCubeTan.position.z);
+
+        // print
+        txtSin.text = fSin.ToString("0.0000");
+        txtCos.text = fCos.ToString("0.0000");
+        txtTan.text = fTan.ToString("0.0000");
+    }
+}
